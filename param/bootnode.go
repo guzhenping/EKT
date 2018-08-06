57475c8ef18216ec63148d5d9ef596203b14f59e
EducationEKT <xlgfzh@gmail.com>
2018-08-06 17:45:41 +0800

    修改peerId逻辑，不存在独立的peerId, 与用户的私钥和地址公用同一套逻辑

--- param/bootnode.go
+++ param/bootnode.go
@@ -6,11 +6,11 @@ import (
 )
 
 var mapping = make(map[string][]p2p.Peer)
-var MainChainDPosNode []p2p.Peer
+var MainChainDelegateNode []p2p.Peer
 
 func InitBootNodes() {
 	mapping["mainnet"] = MainNet
 	mapping["testnet"] = TestNet
 	mapping["localnet"] = LocalNet
-	MainChainDPosNode = mapping[conf.EKTConfig.Env]
+	MainChainDelegateNode = mapping[conf.EKTConfig.Env]
 }
