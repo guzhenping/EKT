6ef7d9404de6d95c0ddad381a75ea47f26f7329b
EducationEKT <xlgfzh@gmail.com>
2018-08-29 21:41:10 +0800

    修改委托人节点

--- param/mainnet.go
+++ param/mainnet.go
@@ -4,5 +4,21 @@ import (
 	"github.com/EducationEKT/EKT/p2p"
 )
 
-// MainNet后面将通过testnet投票产生
-var MainNet = []p2p.Peer{}
+// 委托人节点由官方暂时托管
+var MainNet = []p2p.Peer{
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "58.83.147.171", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "58.83.148.230", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "58.83.148.232", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "58.83.147.172", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.197", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.198", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.200", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.201", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.203", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.204", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.205", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.206", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.207", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.208", 19951, 4},
+	{"1ba20e0b391af81ca227efc4329ecad777cbe5c72dfdbebde04f8fac61ec6eae", "124.251.0.230", 19951, 4},
+}
